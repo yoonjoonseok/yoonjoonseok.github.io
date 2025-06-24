42fa6e977c9cd3af3fb670eac059448801bd30e0
yoonjoonseok <81091628+yoonjoonseok@users.noreply.github.com>
2025-06-24 09:24:31 +0900

    Update commonScript.js

--- commonScript.js
+++ commonScript.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.getElementById("navbar");
+  const loader = document.getElementByClass("loader");
   if (nav) {
     fetch("/common/navbar/navbar.html")
       .then(res => res.text())
       .then(html => {
         nav.innerHTML = html;
+      });
+  }
+  if (loader) {
+    fetch("/common/loader/loader.html")
+      .then(res => res.text())
+      .then(html => {
+        loader.innerHTML = html;
       });
   }
 });
