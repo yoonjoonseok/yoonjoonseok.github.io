9f9ed0f205d0445781c6d06e3549f048ec4844ab
yoonjoonseok <81091628+yoonjoonseok@users.noreply.github.com>
2025-08-06 21:22:10 +0900

    Update flappybird.js

--- game/flappybird.js
+++ game/flappybird.js
@@ -36,7 +36,7 @@ let gravity = 0.4;
 
 let gameOver = false;
 let score = 0;
-var highScore = localStorage.getItem('highScore') === null ? 0 : localStorage.getItem('highScore');
+var highScore = localStorage.getItem('highScore') == null ? 0 : localStorage.getItem('highScore');
 document.getElementById("score").textContent = highScore;
 
 window.onload = function() {
