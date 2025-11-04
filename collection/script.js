4a13e4328b2ef6a1dcc4064524bd49bee9519937
yoonjoonseok <81091628+yoonjoonseok@users.noreply.github.com>
2025-11-04 20:34:06 +0900

    Create script.js

---
+++ collection/script.js
@@ -0,0 +1,93 @@
+var arr = [
+  ['와리오1','닌텐도','DS','DS소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['와리오2','닌텐도','Wii','Wii소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['가리오3','닌텐도','DS','DS소프트웨어', '2001-03-01',33000,'KR','미보유','N','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['나리오4','닌텐도','DS','DS소프트웨어','2001-02-01',33000,'KR','개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['와리오1','닌텐도','DS','DS소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['와리오2','닌텐도','Wii','Wii소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['가리오3','닌텐도','DS','DS소프트웨어', '2001-03-01',33000,'KR','미보유','N','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['나리오4','닌텐도','DS','DS소프트웨어','2001-02-01',33000,'KR','개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ]; 
+  function Item(name, majorCategory, middleCategory, minorCategory, releaseDate, price, nation, status, isCollected, remarks, imageUrl){
+        this.name = name;
+        this.majorCategory = majorCategory;
+        this.middleCategory = middleCategory;
+        this.minorCategory = minorCategory;
+        this.releaseDate = releaseDate;
+        this.price = price;
+        this.nation = nation;
+        this.status = status;
+        this.isCollected = isCollected;
+        this.remarks = remarks;
+        this.imageUrl = imageUrl;
+    };
+  
+  var itemList = arr.map(item => new Item(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7], item[8], item[9], item[10]));
+  
+  const selectElement = document.getElementById('for');
+  const selectSortElement = document.getElementById('selectSort');
+  const isCollectedCheckbox = document.getElementById('isCollectedCheckbox');
+  const statusCheckbox = document.getElementById('statusCheckbox');
+  const resultsContainer = document.getElementById('results');
+  
+  var selectedOptions;
+  var majorCategory;
+  var middleCategory;
+  var minorCategory;
+  var filteredData;
+
+  function filterAndDisplay(){
+    selectedOptions = Array.from(selectElement.options).filter(option => option.selected);
+    majorCategory = selectedOptions.filter(option => option.getAttribute('data-name') === 'majorCategory').map(item => item.value); 
+    middleCategory = selectedOptions.filter(option => option.getAttribute('data-name') === 'middleCategory').map(item => item.value);
+    minorCategory = selectedOptions.filter(option => option.getAttribute('data-name') === 'minorCategory').map(item => item.value);
+    filteredData = itemList.filter(item => majorCategory.includes(item.majorCategory) || middleCategory.includes(item.middleCategory) || minorCategory.includes(item.minorCategory));
+
+    if(isCollectedCheckbox.checked){
+      filteredData = filteredData.filter(item => item.isCollected === 'Y');
+    }
+
+    if(statusCheckbox.checked){
+      filteredData = filteredData.filter(item => item.status === '미개봉');
+    }
+
+    switch (selectSortElement.value){
+      case 'releaseDateAsc' :
+        filteredData = filteredData.toSorted((a,b) => a.releaseDate.localeCompare(b.releaseDate));
+        break; 
+      case 'releaseDateDesc' :
+        filteredData = filteredData.toSorted((a,b) => b.releaseDate.localeCompare(a.releaseDate));
+        break;
+      case 'nameAsc' :
+        filteredData = filteredData.toSorted((a,b) => a.name.localeCompare(b.name));
+        break;
+      case 'nameDesc' :
+        filteredData = filteredData.toSorted((a,b) => b.name.localeCompare(a.name));
+        break;
+    }
+
+    displayResults(filteredData);
+  }
+  
+  function displayResults(data) {
+    resultsContainer.innerHTML = '';
+    data.forEach(item => {
+        const div = document.createElement('div');
+        let imgEffect = (item.status==='미보유')?`filter: grayscale(100%);`:``;
+        let textEffect = (item.status==='미개봉')?`background: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
+                                                  -webkit-background-clip: text;
+                                                  background-clip: text;
+                                                  color: transparent;
+                                                  font-weight: bold;`:``;
+        div.innerHTML = `<div style="background-color: #f0f0f0;border-radius: 10px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);padding:10px">
+    	                     <img style="width:150px;height:150px;` + imgEffect +
+                           `"src="${item.imageUrl}"/><div style="width:200px;` + textEffect + `">${item.name}</div></div>`;
+        resultsContainer.appendChild(div);
+    });
+}
+  
+  selectElement.addEventListener('change', filterAndDisplay);
+  selectSortElement.addEventListener('change', filterAndDisplay);
+  isCollectedCheckbox.addEventListener('change', filterAndDisplay);
+  statusCheckbox.addEventListener('change', filterAndDisplay);
+  filterAndDisplay();
