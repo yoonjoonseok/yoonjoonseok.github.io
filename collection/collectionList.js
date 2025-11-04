312c9f1277738083d753b4f0e740e396d9ba516d
yoonjoonseok <81091628+yoonjoonseok@users.noreply.github.com>
2025-11-04 20:37:57 +0900

    Create collectionList.js

---
+++ collection/collectionList.js
@@ -0,0 +1,10 @@
+var arr = [
+  ['와리오1','닌텐도','DS','DS소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['와리오2','닌텐도','Wii','Wii소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['가리오3','닌텐도','DS','DS소프트웨어', '2001-03-01',33000,'KR','미보유','N','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['나리오4','닌텐도','DS','DS소프트웨어','2001-02-01',33000,'KR','개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['와리오1','닌텐도','DS','DS소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['와리오2','닌텐도','Wii','Wii소프트웨어','2001-01-01',33000,'KR','미개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['가리오3','닌텐도','DS','DS소프트웨어', '2001-03-01',33000,'KR','미보유','N','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ['나리오4','닌텐도','DS','DS소프트웨어','2001-02-01',33000,'KR','개봉','Y','-','https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn.crowdpic.net%2Fdetail-thumb%2Fthumb_d_FCB34C7A85965ECE0F8E24A21DE2176D.png&type=a340'],
+  ]; 
