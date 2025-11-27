ab5e7e5ecf23a1e6d79831a5c9d9155e7890e9e7
yoonjoonseok <yjsygm@naver.com>
2025-11-27 20:43:49 +0900

    a

--- collection/script.js
+++ collection/script.js
@@ -168,15 +168,12 @@ function convertToHierarchicalMap(obj) {
 function renderAllCategory() {
   selectElement.options.length = 0;
   categoryMap.forEach((majorValue, majorKey) => {
-    console.log(majorKey + " " + majorValue);
     renderCategory(majorKey, majorValue, null, null);
     if (majorValue.son instanceof Map) {
       majorValue.son.forEach((middleValue, middleKey) => {
-        console.log(middleKey + " " + middleValue);
         renderCategory(middleKey, middleValue, majorKey, null);
         if (middleValue.son instanceof Map) {
           middleValue.son.forEach((minorValue, minorKey) => {
-            console.log(minorKey + " " + minorValue);
             renderCategory(minorKey, minorValue, majorKey, middleKey);
           })
         }
